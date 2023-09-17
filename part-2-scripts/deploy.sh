3857136528cce1def982af9beeebdba6ebd4830b
Lavpreet Sandhu <lavpreetsandhu@Lavpreets-MacBook-Pro.local>
2023-09-17 09:48:52 +0530

    fun

--- part-2-scripts/deploy.sh
+++ part-2-scripts/deploy.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-export PATH=$PATH:/home/ubuntu/.nvm/versions/node/v20.5.0/bin
+export PATH=$PATH:/home/ubuntu/.nvm/versions/node/v20.6.1/bin
 
 cd Week-5-with-axios-and-backend
  git pull origin master
  cd server
  pm2 kill
  pm2 start index.js
+
+ #just for fun
--- part-2-scripts/script-local.sh
+++ part-2-scripts/script-local.sh
@@ -1,1 +1,3 @@
 ssh -t -i "cert-file.cer" ubuntu@ec2-url  "sudo bash ~/deploy.sh"
+
+ssh -t -i "todo-app-class-se.pem" ubuntu@ec2-54-206-85-198.ap-southeast-2.compute.amazonaws.com "sudo bash ~/deploy.sh"
